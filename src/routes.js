5237e04d8e3d9a9304d1c293560bb8d6be79c185
santiago serna <sanserve91@gmail.com>
2017-05-11 03:12:45 -0500

    Se añaden archivo de rutas para establecer vistas de la aplicacion y su componente relacionado.

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import Home from './components/Home.vue';
+import CharacterDetail from './components/CharacterDetail.vue';
+
+export const routes = [{
+    path: '/',
+    component: Home
+}, {
+    path: '/character_detail',
+    component: CharacterDetail
+}];
