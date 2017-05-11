a93334f84fc75bd69711b2f53c328892e5bc8415
santiago serna <sanserve91@gmail.com>
2017-05-11 03:07:50 -0500

    - Se define main layout de la aplicacion. - Se crea componente header (buscador principal). - Se crea componente vista previa personaje. - Se crea componente comic favorito.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
-    <div id="app">
-        <h1>HOLA</h1>
-    </div>
+    <main id="app">
+        <!-- HEADER -->
+        <div class="row">
+            <header class="main-header">
+                <div class="main-header__controlls">
+                    <input type="text" placeholder="Search character...">
+                </div>
+            </header>
+        </div>
+
+        <!-- MAIN LAYOUT -->
+        <div class="row small-collapse">
+            <router-view></router-view>
+        </div>
+    </main>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'app',
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
     }
-  }
 }
 </script>
-
-<style lang="scss">
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
+// core imports
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+// routes
+import {routes} from './routes';
+
+// main app
 import App from './App.vue';
+
+// load styles
+import mainStyles from './styles/main.scss';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes
+});
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
+    router,
     render: h => h(App)
 });
